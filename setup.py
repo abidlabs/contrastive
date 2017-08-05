4702f0e31a22a4c99857513273adb888ce8ac9c8
Abubakar Abid <aabid@DESKTOP-ROLE13U.stanford.edu>
2017-08-05 16:34:21 +0800

    added archive tag

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ except ImportError:
 
 setup(
     name = 'contrastive',
-    version = '1.0.0',
+    version = '0.1',
     description = 'Python library for performing unsupervised learning (e.g. PCA) in contrastive settings, where one is interested in patterns that exist one dataset, but not the other',
     author = 'Abubakar Abid',
     author_email = 'a12d@stanford.edu',
     url = 'https://github.com/abidlabs/contrastive', 
-    download_url = '',
+    download_url = 'https://github.com/abidlabs/contrastive/archive/0.1.tar.gz',
     packages=['contrastive'],
     keywords = ['unsupervised', 'contrastive', 'learning','PCA'],
     install_requires=[
