facaba4525089f08f2a4c5dbceb3224fd86d4588
Abubakar Abid <a12d@stanford.edu>
2017-12-16 00:44:45 -0800

    Fixed cpca preprocessing issue

--- contrastive/__init__.py
+++ contrastive/__init__.py
@@ -55,7 +55,7 @@ class CPCA(object):
         """
     def fit_transform(self, foreground, background, plot=False, gui=False, alpha_selection='auto', n_alphas=40,  max_log_alpha=3, n_alphas_to_return=4, active_labels = None, colors=None, legend=None, alpha_value=None, return_alphas=False):
         self.fit(foreground, background)
-        return self.transform(dataset=foreground, alpha_selection=alpha_selection,  n_alphas=n_alphas, max_log_alpha=max_log_alpha, n_alphas_to_return=n_alphas_to_return, plot=plot, gui=gui, active_labels=active_labels, colors=colors, legend=legend, alpha_value=alpha_value, return_alphas=return_alphas)
+        return self.transform(dataset=self.fg, alpha_selection=alpha_selection,  n_alphas=n_alphas, max_log_alpha=max_log_alpha, n_alphas_to_return=n_alphas_to_return, plot=plot, gui=gui, active_labels=active_labels, colors=colors, legend=legend, alpha_value=alpha_value, return_alphas=return_alphas)
         
         """
         Computes the covariance matrices of the foreground and background datasets
@@ -424,3 +424,34 @@ class Kernel_CPCA(CPCA):
         K_center[n:,:][:,0:n]=Kyx - np.ones([m,m]).dot(Kyx)/m - Kyx.dot(np.ones([n,n]))/n\
         +np.ones([m,m]).dot(Kyx).dot(np.ones([n,n]))/m/n
         return K_center    
+
+N = 401; D = 1001; gap=3
+# In B, all the data pts are from the same distribution, which has different variances in three subspaces.
+B = np.zeros((N, D))
+B[:,0:10] = np.random.normal(0,10,(N,10))
+B[:,10:20] = np.random.normal(0,3,(N,10))
+B[:,20:30] = np.random.normal(0,1,(N,10))
+
+
+# In A there are four clusters.
+A = np.zeros((N, D))
+A[:,0:10] = np.random.normal(0,10,(N,10))
+# group 1
+A[0:100, 10:20] = np.random.normal(0,1,(100,10))
+A[0:100, 20:30] = np.random.normal(0,1,(100,10))
+# group 2
+A[100:200, 10:20] = np.random.normal(0,1,(100,10))
+A[100:200, 20:30] = np.random.normal(gap,1,(100,10))
+# group 3
+A[200:300, 10:20] = np.random.normal(2*gap,1,(100,10))
+A[200:300, 20:30] = np.random.normal(0,1,(100,10))
+# group 4
+A[300:400, 10:20] = np.random.normal(2*gap,1,(100,10))
+A[300:400, 20:30] = np.random.normal(gap,1,(100,10))
+A_labels = [0]*100+[1]*100+[2]*100+[3]*100
+
+cpca = CPCA(standardize=False)
+cpca.fit_transform(A, B, plot=True, active_labels=A_labels)
+
+print(A.shape)
+print(B.shape)
